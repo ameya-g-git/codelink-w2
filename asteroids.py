a4a48537749a3945569a865be6d336a21ecdd22b
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-01-17 20:41:53 -0500

    boilerplate file done

--- asteroids.py
+++ asteroids.py
@@ -6,12 +6,21 @@ py.init()
 py.display.set_caption("Asteroids")
 clock = py.time.Clock()
 
+# Screen Setup
 w, h = 1200, 800
-counter = 0
 screen = py.display.set_mode((w, h))
 
-white = (242, 242, 242)
-black = (17, 17, 17)
+# Movement & Speed
+x, y = 400, 400
+x_speed, y_speed = 0, 0
+vel = 0
+acceleration = 0.005  # Rate of acceleration increase
+arrow_angle = 0
+deceleration = 0.98  # Adjust the deceleration rate as needed
+
+# Colours
+white = (242,242,242)
+black = (17,17,17)
 
 run_program = True
 while run_program:
@@ -24,7 +33,14 @@ while run_program:
             if event.key == py.K_ESCAPE:
                 run_program = False
 
-py.display.update()
-clock.tick(100)
+    surface = py.Surface((50, 50))
+    surface.fill((255,255,255))
+
+    rotated_surface = py.transform.rotate(surface, arrow_angle)
+    rotated_rect = rotated_surface.get_rect(center = (x, y))
+    screen.blit(rotated_surface, (rotated_rect))
+
+    py.display.update()
+    clock.tick(100)
 
 py.quit()
